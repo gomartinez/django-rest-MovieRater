928ba041a861420587932c652dc8637150ef3989
gomartinez <gonzalo.martinez91@gmail.com>
2020-05-23 18:41:12 -0300

    First commit

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/gonzalomartinez/Documents/Gonzalo/Python/MovieRaterApi/venv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
